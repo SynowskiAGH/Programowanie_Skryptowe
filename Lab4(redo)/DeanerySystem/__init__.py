58dc606da339f4ef43c013c0d87e2e98314173ae
SynowskiAGH <92153782+SynowskiAGH@users.noreply.github.com>
2021-11-17 21:41:57 +0100

    Create __init__.py

---
+++ Lab4(redo)/DeanerySystem/__init__.py
@@ -0,0 +1,6 @@
+from DeanerySystem.lesson import Lesson
+from DeanerySystem.day import Day, nthDayFrom
+from DeanerySystem.term import Term
+from DeanerySystem.action import Action
+from DeanerySystem.Timetable1 import Timetable1
+from DeanerySystem.teacher import Teacher
