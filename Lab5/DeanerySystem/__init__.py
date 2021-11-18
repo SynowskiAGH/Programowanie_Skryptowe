b65c5f253e89d89022e4d8822951fea0aaf4f7dc
SynowskiAGH <92153782+SynowskiAGH@users.noreply.github.com>
2021-11-18 23:51:10 +0100

    Update __init__.py

--- Lab5/DeanerySystem/__init__.py
+++ Lab5/DeanerySystem/__init__.py
@@ -5,3 +5,6 @@ from DeanerySystem.action import Action
 from DeanerySystem.Timetable1 import Timetable1
 from DeanerySystem.teacher import Teacher
 from DeanerySystem.break_class import Break
+from DeanerySystem.basicterm import BasicTerm
+from DeanerySystem.basictimetable import BasicTimetable
+from DeanerySystem.Timetable2 import Timetable2
