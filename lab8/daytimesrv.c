05795720efefcae3a77eba2235c83fd001b1f792
SynowskiAGH <92153782+SynowskiAGH@users.noreply.github.com>
2023-03-12 00:12:33 +0100

    Update daytimesrv.c

--- lab8/daytimesrv.c
+++ lab8/daytimesrv.c
@@ -1,41 +1,38 @@
-#include        <sys/types.h>   /* basic system data types */
-#include        <sys/socket.h>  /* basic socket definitions */
-#include        <sys/time.h>    /* timeval{} for select() */
-#include        <time.h>                /* timespec{} for pselect() */
-#include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
-#include        <arpa/inet.h>   /* inet(3) functions */
-#include        <errno.h>
-#include        <fcntl.h>               /* for nonblocking */
-#include        <netdb.h>
-#include        <signal.h>
-#include        <stdio.h>
-#include        <stdlib.h>
-#include        <string.h>
-#include        <unistd.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAXLINE 1024
 
-//#define SA struct sockaddr
-
 #define LISTENQ 4
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     int listenfd, connfd;
     socklen_t len;
-    char buff[MAXLINE], str[INET6_ADDRSTRLEN + 1];
+    char buff[MAXLINE], str[INET6_ADDRSTRLEN + 1], username[MAXLINE];
     time_t ticks;
     struct sockaddr_in6 servaddr, cliaddr;
 
     if ((listenfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
-        fprintf(stderr, "socket error : %s\n", strerror(errno));
+        fprintf(stderr, "socket error: %s\n", strerror(errno));
         return 1;
     }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin6_family = AF_INET6;
+
     if (argc == 1)
         servaddr.sin6_addr = in6addr_any;
     else {
@@ -44,42 +41,50 @@ main(int argc, char **argv)
             return -1;
         }
     }
-    servaddr.sin6_port = htons(13);    /* daytime server */
 
-    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        fprintf(stderr, "bind error : %s\n", strerror(errno));
+    servaddr.sin6_port = htons(13); /* daytime server */
+
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        fprintf(stderr, "bind error: %s\n", strerror(errno));
         return 1;
     }
 
     if (listen(listenfd, LISTENQ) < 0) {
-        fprintf(stderr, "listen error : %s\n", strerror(errno));
+        fprintf(stderr, "listen error: %s\n", strerror(errno));
         return 1;
     }
 
-    fprintf(stderr, "Waiting for clients ... \n");
+    fprintf(stderr, "Waiting for clients ...\n");
+
     for (;;) {
         len = sizeof(cliaddr);
-        if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) < 0) {
-            fprintf(stderr, "accept error : %s\n", strerror(errno));
+        if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &len)) < 0) {
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
             continue;
         }
 
         bzero(str, sizeof(str));
-        inet_ntop(AF_INET6, (struct sockaddr *) &cliaddr.sin6_addr, str, sizeof(str));
+        inet_ntop(AF_INET6, (struct sockaddr *)&cliaddr.sin6_addr, str, sizeof(str));
         printf("Connection from %s\n", str);
 
-        char username[MAXLINE + 1];
-        if (read(connfd, username, MAXLINE) < 0) {
-            fprintf(stderr, "read error : %s\n", strerror(errno));
-        } else {
-            username[strlen(username) - 1] = '\0'; // remove the newline character
-            printf("Received username: %s\n", username);
+        bzero(username, sizeof(username));
+        if (read(connfd, username, sizeof(username) - 1) <= 0) {
+            fprintf(stderr, "read error: %s\n", strerror(errno));
+            close(connfd);
+            continue;
         }
 
+        printf("Username: %s", username);
+
         ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "Hello %s, the current time is %.24s\r\n", username, ctime(&ticks));
-        if (write(connfd, buff, strlen(buff)) < 0)
-            fprintf(stderr, "write error : %s\n", strerror(errno));
+        snprintf(buff, sizeof(buff), "Current time: %.24s\r\n", ctime(&ticks));
+
+        if (write(connfd, buff, strlen(buff)) != (ssize_t)strlen(buff)) {
+            fprintf(stderr, "write error: %s\n", strerror(errno));
+        }
+
         close(connfd);
     }
+
+    return 0;
 }
